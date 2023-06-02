ba0013673c6fdf129842db504d294c7038bfc23a
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-06-02 17:46:05 -0300

    test with 22/7 Pi aproximation

--- uq64x64/src/test.rs
+++ uq64x64/src/test.rs
@@ -1,3 +1,11 @@
+/*
+Inspired in:
+https://ethereum.stackexchange.com/questions/113130/what-does-decode112with18-do
+https://github.com/Uniswap/v2-core/blob/master/contracts/libraries/UQ112x112.sol
+
+
+*/
+
 #![cfg(test)]
 
 use super::{UQ64x64Contract, UQ64x64ContractClient};
@@ -13,19 +21,27 @@ fn test() {
 
     const Q64: u128 = 2u128.pow(64);
 
+    /*
+    For this test we will use 22/7 (Pi aproximation)
+    22/7 ≈ 3.142857142857143
+    
+    */
+
     let input: u64 = 10;
     let expected: u128 = 10 * Q64;
     let result = client.encode(&input);
     assert_eq!(result, expected);
 
     // Here we want to divide 10/3 which should be 3.33333
-    /*
-    
-    */
-    let x: u128 = 10 * Q64;
-    let y: u64 = 3;
-    let expected_div: u128 = 10 * Q64 / 3;
-    let result_div = client.uqdiv(&x, &y);
+    let x: u64 = 22;
+    let y: u64 = 7;
+    let x_encoded: u128 = client.encode(&x);
+    assert_eq!(x_encoded, 22 * Q64);
+
+    let y_into: u128 = y.into();
+    let expected_div: u128 = x_encoded / y_into;
+
+    let result_div = client.uqdiv(&x_encoded, &y);
     /*  This will produce a fixed point unsigned Q64x64
         number that represents 1/3. This number is 
         represented as a u128 where the upper 64 bits
@@ -34,19 +50,13 @@ fn test() {
 
     assert_eq!(result_div, expected_div);
 
-    let integer_part: u64 = (result_div >> 64) as u64;
-    //let fractional_mask: u128 = (1u128 << 64) - 1;
-    //let fractional_part: u64 = (result_div & fractional_mask) as u64;
-    let fractional_part: u64 = (result_div << 65 ) as u64;
-
+    let integer_part: u64 = client.integer_part(&result_div);
     assert_eq!(integer_part, 3);
-    assert_eq!(fractional_part, 3333);
 
-
-
-    // The result is not "just" 10/3 converted
-    //let new_input: u64 = 10/3;
-   // assert_eq!(new_input, 1);
+    
+    let decoded_with_7_decimals = client.decode_with_7_decimals(&result_div);
+    // 22/7 ≈ 3.1428571
+    assert_eq!(decoded_with_7_decimals, 31428571);
 
 
 }
